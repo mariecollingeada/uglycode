3e9e84facdd38c8c0005fb18f0d4c4e2c5ab55d2
Steve Rich <steven.john.rich@gmail.com>
2024-02-01 19:31:11 +0000

    Update CalculatorTest.java

--- src/test/java/uk/ada/uglycode/CalculatorTest.java
+++ src/test/java/uk/ada/uglycode/CalculatorTest.java
@@ -4,33 +4,28 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CalculatorTest {
+public class hltchkTest {
 
     @Test
     public void testStringInputWithAddition() {
         String input = "5 5 +";
-        int result = Calculator.calculate(input);
+        int result = hltchk.cmp(input);
         assertEquals(10, result);
     }
 
     @Test
     public void testStringInputWithMultiplication() {
         String input = "5 5 *";
-        int result = Calculator.calculate(input);
+        int result = hltchk.cmp(input);
         assertEquals(25, result);
     }
 
     @Test
     public void testStringInputWithSubtraction() {
         String input = "5 5 -";
-        int result = Calculator.calculate(input);
+        int result = hltchk.cmp(input);
         assertEquals(0, result);
     }
 
-    @Test
-    public void testStringInputWithErrorSymbol() {
-        String input = "5 5 #";
-        int result = Calculator.calculate(input);
-        assertEquals(5, result);
-    }
+
 }
